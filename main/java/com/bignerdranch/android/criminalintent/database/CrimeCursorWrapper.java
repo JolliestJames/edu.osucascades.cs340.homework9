b343b2ae079093dbda38be32d359d2cc0f232cbb
ToBeAmped <martinja@oregonstate.edu>
2018-03-10 17:13:04 -0800

    Finish implementation of getCrime method

--- main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
+++ main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -6,6 +6,9 @@ import android.database.CursorWrapper;
 import com.bignerdranch.android.criminalintent.Crime;
 import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class CrimeCursorWrapper extends CursorWrapper {
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
@@ -17,7 +20,12 @@ public class CrimeCursorWrapper extends CursorWrapper {
         long date = getLong(getColumnIndex((CrimeTable.Columns.DATE)));
         int isSolved = getInt(getColumnIndex(CrimeTable.Columns.SOLVED));
 
-        return null;
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
     }
 
 }
